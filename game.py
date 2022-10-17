0dae03ec222e390d0f2a14ad507958a8107ee3b7
KokoseiJ <kokoseij@gmail.com>
2022-10-17 13:50:10 -0700

    Implemented basic game classes

---
+++ game.py
@@ -0,0 +1,105 @@
+import pygame
+
+import logging
+from queue import Queue
+from threading import Event
+
+logger = logging.getLogger("RHYTHMATICA")
+
+
+class Scene:
+    """
+    Class for determining each scenes in game and how the game should act.
+    """
+    name = "Scene"
+
+    def __init__(self):
+        self.game = None
+
+    def set_game(self, game):
+        self.game = game
+
+    def start(self):
+        raise NotImplementedError()
+
+    def handle_event(self, event):
+        pass
+
+    def cleanup(self):
+        pass
+
+
+class Game:
+    def __init__(self, size=None, name="RHYTHMATICA", fps=60, scene=None):
+        self.size = size if size is not None else (1280, 720)
+        self.name = name
+        self.fps = fps
+
+        self.screen = None
+        self.clock = None
+
+        self.tasks = Queue()
+        self.stop_flag = Event()
+        self.scene = scene
+
+        logger.info("Hello, World!")
+
+    def init_pygame(self):
+        logger.info("Initializing %s...", self.name)
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        self.screen.set_caption(self.name)
+        logger.debug("Display Spawned!")
+
+        self.clock = pygame.time.Clock()
+        logger.debug("Clock is up and running!")
+
+    def set_scene(self, scene):
+        if not type(scene) is Scene:
+            raise TypeError(f"Scene {scene} is not callable!")
+
+        if not isinstance(scene, Scene):
+            scene = scene()
+
+        logger.debug("Calling cleanup on Scene %s", self.scene)
+        self.scene.cleanup()
+
+        name = scene.name
+        logger.debug("Registering %s as a scene", name)
+        self.scene = scene
+        self.scene.set_game(self)
+        logger.debug("Scene registered, Calling start", name)
+        self.scene.start()
+        logger.debug("Scene started", name)
+
+    def add_task(self, task):
+        """
+        Task should be a callable with one argument.
+        """
+        self.tasks.put(task)
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            logger.warning("pygame.QUIT triggered. Stopping the loop..")
+            self.stop()
+
+    def run(self):
+        if self.scene is None:
+            logger.error("I refuse to run the game without a scene!")
+            raise RuntimeError("Scene should be set before running the game")
+
+        logger.info("Running the game!")
+        while not self.stop_flag.is_set():
+            for event in pygame.event.get():
+                self.handle_event()
+                self.scene.handle_event()
+
+            while not self.tasks.empty():
+                func = self.tasks.get()
+                func(self)
+
+            pygame.display.flip()
+            self.clock.tick(self.fps)
+
+    def stop(self):
+        self.stop_flag.set()
