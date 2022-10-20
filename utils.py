58e776d705591ec3a937a3360f385a194b6b02d6
KokoseiJ <kokoseij@gmail.com>
2022-10-19 22:45:47 -0700

    Added SmoothMoveXY

--- utils.py
+++ utils.py
@@ -1,6 +1,72 @@
 import pygame
 
+import time
 import random
+
+
+class SmoothMoveXY:
+    def __init__(self, surface, orig, dest, duration):
+        self.surface = surface
+        self.orig = orig
+        self.dest = dest
+        self.duration = duration
+        self.starttime = None
+
+        self.distance_x = dest[0] - orig[0]
+        self.start_speed_x = 2 * self.distance_x / duration
+        self.acceleration_x = -self.start_speed_x / duration
+
+        self.distance_y = dest[1] - orig[1]
+        self.start_speed_y = 2 * self.distance_y / duration
+        self.acceleration_y = -self.start_speed_y / duration
+
+    @property
+    def is_started(self):
+        return self.starttime is not None
+
+    @property
+    def elapsed_time(self):
+        if self.is_started:
+            return time.perf_counter() - self.starttime
+        else:
+            return None
+
+    @property
+    def is_running(self):
+        return self.is_started and not self.elapsed_time > self.duration
+
+    def start(self):
+        self.starttime = time.perf_counter()
+
+    def reset(self):
+        self.starttime = None
+
+    def get_pos(self):
+        return self._get_pos(self.elapsed_time)
+
+    def draw(self, surface):
+        return surface.blit(self.surface, self.get_pos())
+
+    def _get_pos(self, time_):
+        if not self.is_started:
+            cur_pos = self.orig
+        elif self.elapsed_time <= self.duration:
+            # s = v0t + 1/2at^2
+            cur_pos_x = (
+                self.orig[0]
+                + self.start_speed_x * time_
+                + self.acceleration_x * time_**2 / 2
+            )
+            cur_pos_y = (
+                self.orig[1]
+                + self.start_speed_y * time_
+                + self.acceleration_y * time_**2 / 2
+            )
+            cur_pos = (cur_pos_x, cur_pos_y)
+        else:
+            cur_pos = self.dest
+
+        return cur_pos
 
 
 def calc_size_rel(full_y, size, factor):
