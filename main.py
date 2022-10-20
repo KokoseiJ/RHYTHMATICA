cd81a4a3daf100b0066a3110c450eb98c8890c88
KokoseiJ <kokoseij@gmail.com>
2022-10-19 18:29:19 -0700

    Added log formatter

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ import logging
 
 logger = logging.getLogger("RHYTHMATICA")
 logger.setLevel("DEBUG")
+
 handler = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(module)s::%(funcName)s | %(message)s",
+    "%Y/%m/%d %H:%M:%S"
+)
+
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-game = Game((1600, 900))
+game = Game((1920, 1080), fps=0, show_fps=True)
 game.init_pygame()
 game.set_scene(Play)
 game.run()
