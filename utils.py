1e95237e5e39d93fff94dffe760d5a5b22e06fcd
KokoseiJ <kokoseij@gmail.com>
2022-10-19 22:54:03 -0700

    utils use unified logics to calculate locations now

--- utils.py
+++ utils.py
@@ -80,14 +80,20 @@ def calc_size_rel(full_y, size, factor):
     return (neww, newh)
 
 
+def calc_center(srcsize, loc):
+    w, h = srcsize
+    return (loc[0] - w / 2, loc[1] - h / 2)
+
+
+def calc_loc_rel(full, factors):
+    return (full[0] * factors[0], full[1] * factors[1])
+
+
 def scale_rel(surface, factor, maxsize=None, func=None):
     if maxsize is None:
         maxsize = pygame.display.get_window_size()
     if func is None:
         func = pygame.transform.smoothscale
-
-    if factor > 1:
-        factor /= 100
 
     newsize = calc_size_rel(maxsize[1], surface.get_size(), factor)
 
@@ -95,24 +101,12 @@ def scale_rel(surface, factor, maxsize=None, func=None):
 
 
 def blit_center(dest, src, loc, *args, **kwargs):
-    w, h = src.get_size()
-    x, y = loc
-
-    newx = x - w / 2
-    newy = y - h / 2
-
-    return dest.blit(src, (newx, newy), *args, **kwargs)
+    return dest.blit(src, calc_center(src.get_size(), loc), *args, **kwargs)
 
 
 def blit_center_rel(dest, src, factors, *args, **kwargs):
-    srcw, srch = dest.get_size()
-    w, h = src.get_size()
-    fx, fy = factors
-
-    x = srcw * fx
-    y = srch * fy
-
-    return blit_center(dest, src, (x, y), *args, **kwargs)
+    loc_rel = calc_loc_rel(dest.get_size(), factors)
+    return blit_center(dest, src, loc_rel, *args, **kwargs)
 
 
 def text_multiline(font, text, *args, **kwargs):
