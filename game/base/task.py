2f5952098850d15f5ba9b6faaab1942628ef6cee
KokoseiJ <kokoseij@gmail.com>
2022-10-24 13:49:32 -0700

    Added task classed, not yet applied

---
+++ game/base/task.py
@@ -0,0 +1,53 @@
+import time
+from threading import Event
+
+
+class Task:
+    def __init__(self, func, *args, name=None, **kwargs):
+        self.name = name if name is not None else func.__code__.co_name
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.finish_flag = Event()
+
+        if not callable(func):
+            raise TypeError(f"{func.__code__.co_name} is not callable!")
+
+    @property
+    def is_finished(self):
+        return self.finish_flag.is_set()
+
+    def runagain(self, game):
+        game.add_task(self)
+
+    def run(self, game):
+        self._run(game)
+
+    def _run(self, game):
+        self.func(game, *self.args, **self.kwargs)
+        self.finish_flag.set()
+
+
+class WaitTask(Task):
+    def __init__(self, func, after=0, *args, name=None, **kwargs):
+        super().__init__(func, *args, name=name, **kwargs)
+        self.target_time = time.perf_counter() + after
+
+    def run(self, game):
+        if time.perf_counter() < self.target_time:
+            self.runagain(game)
+
+        else:
+            self._run(game)
+
+
+class WaitForTask(Task):
+    def __init__(self, func, task, *args, name=None, **kwargs):
+        super().__init__(func, *args, name=name, **kwargs)
+        self.task = task
+
+    def run(self, game):
+        if not self.task.is_finished:
+            self.runagain(game)
+        else:
+            self._run(game)
